deab0b82b12ab31f188c9ae2190ef5627dbca3ff
sh-momina <sh.mominaa@gmail.com>
2025-05-20 00:39:31 +0500

    Delete templatetags directory

--- templatetags/costum_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-import base64
-
-register = template.Library()
-
-@register.filter
-def b64encode(value):
-    """Encode binary data to base64."""
-    return base64.b64encode(value).decode('utf-8')
